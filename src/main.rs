3e0609a18439e426828b2d4df992723f57c533ca
Christian Schroeder <cschroeder1338@gmail.com>
2024-06-28 21:22:43 -0400

    add tracks if not already present

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rspotify::{model::PlaylistId, prelude::*, AuthCodeSpotify, Credentials, OAuth, scopes};
+use rspotify::{model::{PlaylistId, TrackId}, prelude::*, scopes, AuthCodeSpotify, Credentials, OAuth};
 use std::collections::HashSet;
 
 async fn build_rspotify_client() -> AuthCodeSpotify {
@@ -14,8 +14,7 @@ async fn build_rspotify_client() -> AuthCodeSpotify {
     spotify
 }
 
-async fn get_playlist_track_ids(spotify: &AuthCodeSpotify, playlist_id: &str) -> HashSet<String> {
-    let playlist_id = PlaylistId::from_id(playlist_id).unwrap();
+async fn get_playlist_track_ids(spotify: &AuthCodeSpotify, playlist_id: &PlaylistId<'_>) -> HashSet<String> {
 
     // we want to use a HashSet because want to store unique ids that we may access frequently
     let mut track_ids = HashSet::new();
@@ -41,12 +40,36 @@ async fn get_playlist_track_ids(spotify: &AuthCodeSpotify, playlist_id: &str) ->
     track_ids
 }
 
+
+pub fn track_id_str_to_playable_id(track_id: &str) -> PlayableId {
+    PlayableId::Track(TrackId::from_id(track_id).unwrap())
+}
+
+async fn add_tracks_to_playlist_if_not_exists(
+    spotify: &AuthCodeSpotify,
+    playlist_id: &str,
+    existing_track_set: HashSet<String>,
+    new_track_ids: Vec<String>,
+) -> () {
+    let tracks_not_in_playlist: Vec<_> = new_track_ids.into_iter().filter(|track_id| !existing_track_set.contains(track_id)).collect();
+    if !tracks_not_in_playlist.is_empty() {
+
+        let tracks_to_add = tracks_not_in_playlist.iter().map(|track_id| track_id_str_to_playable_id(track_id)).collect::<Vec<PlayableId>>();
+
+        let playlist_id = PlaylistId::from_id(playlist_id).unwrap();
+        spotify.playlist_add_items(playlist_id, tracks_to_add, None).await.unwrap();
+    }
+}
+
 #[tokio::main]
 async fn main() {
-    let spotify = build_rspotify_client().await;
+    let spotify: AuthCodeSpotify = build_rspotify_client().await;
 
-    let playlist_id = "7hVMUyFFi6bNtjO4hubtJm"; // Replace with your playlist ID
-    let track_ids = get_playlist_track_ids(&spotify, playlist_id).await;
+    let playlist_id_str: &str = "4wN2XkppT6xRzQI5js4nd3";
+    let playlist_id: PlaylistId = PlaylistId::from_id(playlist_id_str).unwrap();
+    let track_ids: HashSet<String> = get_playlist_track_ids(&spotify, &playlist_id).await;
 
     println!("Number of tracks: {}", track_ids.len());
+
+    add_tracks_to_playlist_if_not_exists(&spotify, playlist_id_str, track_ids, vec!["5iKndSu1XI74U2OZePzP8L".to_string()]).await;
 }
